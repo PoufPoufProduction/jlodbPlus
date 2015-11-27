b1cc9f6d6c45e9fae803da17e61cc69465d62d47
poufpoufproduction <johannc@poufpoufproduction.fr>
2015-11-27 17:12:03 +0100

    it and geometry snapshots

---
+++ res/img/exercices/asm/screenGB.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+echo convert $1
+convert -crop 176x158+461+148 $1 fg.png
+convert -resize 400x fg.png fg.png
+convert -blur 2x3 $1 bg.png
+composite -geometry  +200+40 fg.png bg.png $1
+rm -f fg.png bg.png
