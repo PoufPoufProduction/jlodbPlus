3e0f8ad685c4a24f2645ee36a2589c6a8831df4e
JohannC <johannc@poufpoufproduction.fr>
2020-05-21 09:31:54 +0200

    new snapshots

--- res/img/exercices/asm/reg.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-echo convert $1
-convert -crop 140x88+6+432 $1 fg.png
-convert -adaptive-resize 350x fg.png fg.png
-convert -blur 2x3 $1 bg.png
-composite -geometry  +250+40 fg.png bg.png $1
-rm -f fg.png bg.png tmp.png
